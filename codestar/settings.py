f786fc3ee4103ce41328463220efe35daebedf5a
Matthew Hughes <matt.h17@yahoo.com>
2024-02-01 22:03:33 +0000

    added heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#_wqsl@xcg)0e5vw%4sbfx+#s-0ix9g6x-zf)x!e)ltn3r)_fe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mattthughes-djangoblogg-2asbxyqbtwb.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['8000-mattthughes-djangoblogg-2asbxyqbtwb.ws-eu107.gitpod.io','.herokuapp.com']
 
 
 # Application definition
